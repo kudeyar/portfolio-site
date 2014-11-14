aac12be6be73d85bf715e746ba3d8344487ba8c6
Anton Golomazov <agolomazov@itloft.ru>
2014-11-14 18:40:14 +0300

    add new library to download

--- app/tmpl/footer.php
+++ app/tmpl/footer.php
@@ -11,11 +11,17 @@
 </div> <!-- // Главная обертка -->
 
     <!-- Скрипты только в конце страницы -->
-    <script src="/app/bower_components/jquery/dist/jquery.js"></script>
+ <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- <script src="/app/bower_components/jquery/dist/jquery.js"></script> -->
     <script src="/app/bower_components/bpopup/jquery.bpopup.js"></script>
     <script src="/app/bower_components/qtip2/jquery.qtip.js"></script>
-    <script src="/app/scripts/ajaxupload.js"></script>
-    <script src="/app/scripts/main.js"></script>
+ <!-- The jQuery UI widget factory, can be omitted if jQuery UI is already included -->
+ <script src="/app/scripts/jquery.ui.widget.js"></script>
+ <!-- The Iframe Transport is required for browsers without support for XHR file uploads -->
+ <script src="/app/scripts/jquery.iframe-transport.js"></script>
+ <!-- The basic File Upload plugin -->
+ <script src="/app/scripts/jquery.fileupload.js"></script>
+     <script src="/app/scripts/main.js"></script>
 
 </body>
 </html>
